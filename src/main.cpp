4eca8a3af69fb9d0b177d1d63484383dfe79755c
Sergey Kosarevsky <sk@linderdaum.com>
2015-08-03 17:59:05 +0200

    Play a file from the command line, break on kbhit()

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,32 @@
+#include <chrono>
+#include <thread>
+#include <conio.h>
+
 #include "AudioSubsystem.h"
 #include "AudioSubsystem_OpenAL.h"
 #include "Decoders/iWaveDataProvider.h"
 #include "Decoders/WAV/WAVDataProvider.h"
 #include "Utils.h"
 
-int main()
+int main( int argc, char* argv[] )
 {
+	const char* FileName = ( argc > 1 ) ? argv[1] : "test.wav";
+
 	auto AudioSubsystem = CreateAudioSubsystem_OpenAL();
 
 	AudioSubsystem->Start();
 
-	auto TestBlob = ReadFileAsBlob( "test.wav" );
+	auto TestBlob = ReadFileAsBlob( FileName );
 	auto Provider = std::make_shared<clWAVDataProvider>( TestBlob );
 	auto Source = AudioSubsystem->CreateAudioSource();
 	Source->BindDataProvider( Provider );
 	Source->Play();
 
-	while ( Source->IsPlaying() );
+	while ( Source->IsPlaying() && !_kbhit() );
+
+	std::this_thread::sleep_for( std::chrono::milliseconds(300) );
+
+	Source = nullptr;
 
 	AudioSubsystem->Stop();
 
