518d0a19dc8d826b899270a55ce15a50e8257421
Sergey Kosarevsky <sk@linderdaum.com>
2015-08-04 15:14:40 +0200

    Streaming code works (sort of)

--- src/AudioSubsystem_OpenAL.cpp
+++ src/AudioSubsystem_OpenAL.cpp
@@ -4,12 +4,19 @@
 	Based on https://github.com/corporateshark/Android-NDK-Game-Development-Cookbook/tree/master/Chapter5
 **/
 
+#include <algorithm>
 #include <atomic>
+#include <memory>
+#include <mutex>
 #include <thread>
+#include <vector>
 
 #include "AudioSubsystem_OpenAL.h"
 #include "Decoders/iWaveDataProvider.h"
 #include "OpenAL/LAL.h"
+
+const int BUFFER_DURATION = 250; // milliseconds
+const int BUFFER_SIZE = 44100 * 2 * 2 * BUFFER_DURATION / 1000;
 
 static ALenum ConvertWaveDataFormatToAL( const sWaveDataFormat& F )
 {
@@ -26,11 +33,14 @@ static ALenum ConvertWaveDataFormatToAL( const sWaveDataFormat& F )
 	return AL_FORMAT_MONO8;
 }
 
-class clAudioSource_OpenAL: public iAudioSource
+class clAudioSubsystem_OpenAL;
+
+class clAudioSource_OpenAL: public iAudioSource, public std::enable_shared_from_this<clAudioSource_OpenAL>
 {
 public:
-	clAudioSource_OpenAL()
-	: m_DataProvider()
+	explicit clAudioSource_OpenAL( clAudioSubsystem_OpenAL* AudioSubsystem )
+	: m_AudioSubsystem( AudioSubsystem )
+	, m_DataProvider()
 	, m_BuffersCount(0)
 	, m_SourceID(0)
 	, m_BufferID()
@@ -52,12 +62,59 @@ public:
 	virtual bool IsPlaying() const override;
 
 private:
+	void PrepareBuffers();
+	void UnqueueAllBuffers();
+	void UpdateBuffers();
+	void EnqueueOneBuffer();
+	size_t StreamBuffer( ALuint bufferId, size_t Size );
+	friend class clAudioSubsystem_OpenAL;
+
+private:
+	clAudioSubsystem_OpenAL* m_AudioSubsystem;
 	std::shared_ptr<iWaveDataProvider> m_DataProvider;
 
 	// OpenAL stuff
 	ALsizei m_BuffersCount;
 	ALuint  m_SourceID;
 	ALuint  m_BufferID[2];
+};
+
+class clAudioSubsystem_OpenAL: public iAudioSubsystem
+{
+public:
+	clAudioSubsystem_OpenAL();
+	virtual ~clAudioSubsystem_OpenAL();
+
+	virtual void Start() override;
+	virtual void Stop() override;
+
+	virtual std::shared_ptr<iAudioSource> CreateAudioSource() override;
+
+	virtual void SetListenerGain( float Gain ) override;
+
+private:
+	void DebugPrintVersion();
+	void RegisterSource( const std::shared_ptr<clAudioSource_OpenAL>& Source );
+	void UnregisterSource( clAudioSource_OpenAL* Source );
+	std::vector< std::shared_ptr<clAudioSource_OpenAL> > GetLockedSources()
+	{
+		std::unique_lock<std::mutex> Lock( m_ActiveSourcesMutex );
+
+		return m_ActiveSources;
+	}
+
+	friend class clAudioSource_OpenAL;
+
+private:
+	ALCdevice*  m_Device;
+	ALCcontext* m_Context;
+
+	std::shared_ptr<std::thread> m_AudioThread;
+	std::atomic<bool> m_IsPendingExit;
+	std::atomic<bool> m_IsInitialized;
+
+	std::vector< std::shared_ptr<clAudioSource_OpenAL> > m_ActiveSources;
+	std::mutex m_ActiveSourcesMutex;
 };
 
 void clAudioSource_OpenAL::BindDataProvider( const std::shared_ptr<iWaveDataProvider>& Provider )
@@ -93,11 +150,125 @@ void clAudioSource_OpenAL::BindDataProvider( const std::shared_ptr<iWaveDataProv
 	}
 }
 
+void clAudioSource_OpenAL::PrepareBuffers()
+{
+	if ( !m_DataProvider ) return;
+
+	int State;
+	alGetSourcei( m_SourceID, AL_SOURCE_STATE, &State );
+
+	if ( State != AL_PAUSED && m_DataProvider->IsStreaming() )
+	{
+		UnqueueAllBuffers();
+
+		StreamBuffer( m_BufferID[0], BUFFER_SIZE );
+		StreamBuffer( m_BufferID[1], BUFFER_SIZE );
+
+		alSourceQueueBuffers( m_SourceID, 2, &m_BufferID[0] );
+
+		m_AudioSubsystem->RegisterSource( shared_from_this() );
+	}
+}
+
+void clAudioSource_OpenAL::UnqueueAllBuffers()
+{
+	int NumQueued;
+	alGetSourcei( m_SourceID, AL_BUFFERS_QUEUED, &NumQueued );
+
+	if ( NumQueued > 0 )
+	{
+		alSourceUnqueueBuffers( m_SourceID, NumQueued, &m_BufferID[0] );
+	}
+}
+
+size_t clAudioSource_OpenAL::StreamBuffer( ALuint BufferID, size_t Size )
+{
+	if ( !m_DataProvider ) return 0;
+
+	size_t ActualSize = m_DataProvider->StreamWaveData( Size );
+
+	if ( !ActualSize ) return 0;
+
+	const uint8_t* Data = m_DataProvider->GetWaveData();
+	size_t DataSize = m_DataProvider->GetWaveDataSize();
+	const sWaveDataFormat& Format = m_DataProvider->GetWaveDataFormat();
+
+	alBufferData(
+		BufferID,
+		ConvertWaveDataFormatToAL( Format ),
+		Data,
+		DataSize,
+		Format.m_SamplesPerSecond
+	);
+
+	return ActualSize;
+}
+
+void clAudioSource_OpenAL::EnqueueOneBuffer()
+{
+	ALuint BufID;
+	alSourceUnqueueBuffers( m_SourceID, 1, &BufID );
+
+	size_t Size = this->StreamBuffer( BufID, BUFFER_SIZE );
+
+	if ( m_DataProvider->IsEndOfStream() )
+	{
+		if ( this->IsLooping() )
+		{
+			m_DataProvider->Seek( 0 );
+			if ( !Size ) Size = StreamBuffer( BufID, BUFFER_SIZE );
+		}
+	}
+
+	if ( Size )
+	{
+		alSourceQueueBuffers(m_SourceID, 1, &BufID);
+	}
+	else
+	{
+		this->Stop();
+	}
+}
+
+void clAudioSource_OpenAL::UpdateBuffers()
+{
+	if ( !m_DataProvider ) return;
+
+	if ( m_DataProvider->IsStreaming() )
+	{
+		int NumProcessed = 0;
+		alGetSourcei( m_SourceID, AL_BUFFERS_PROCESSED, &NumProcessed );
+
+		switch ( NumProcessed )
+		{
+		case 0:
+			// nothing was processed
+			break;
+		case 1:
+		{
+			EnqueueOneBuffer();
+			break;
+		}
+		case 2:
+		{
+			EnqueueOneBuffer();
+			EnqueueOneBuffer();
+			alSourcePlay( m_SourceID );
+			break;
+		}
+		default:
+			break;
+		}
+	}
+}
+
 void clAudioSource_OpenAL::Play()
 {
 	if ( this->IsPlaying() ) return;
 
 	if ( !m_DataProvider ) return;
+
+	PrepareBuffers();
 
 	alSourcePlay( m_SourceID );
 }
@@ -105,6 +276,9 @@ void clAudioSource_OpenAL::Play()
 void clAudioSource_OpenAL::Stop()
 {
 	alSourceStop( m_SourceID );
+
+	UnqueueAllBuffers();
+	m_AudioSubsystem->UnregisterSource( this );
 
 	if ( m_DataProvider )
 	{
@@ -120,31 +294,6 @@ bool clAudioSource_OpenAL::IsPlaying() const
 
 	return State == AL_PLAYING;
 }
-
-class clAudioSubsystem_OpenAL: public iAudioSubsystem
-{
-public:
-	clAudioSubsystem_OpenAL();
-	virtual ~clAudioSubsystem_OpenAL();
-
-	virtual void Start() override;
-	virtual void Stop() override;
-
-	virtual std::shared_ptr<iAudioSource> CreateAudioSource() override;
-
-	virtual void SetListenerGain( float Gain ) override;
-
-private:
-	void DebugPrintVersion();
-
-private:
-	ALCdevice*  m_Device;
-	ALCcontext* m_Context;
-
-	std::shared_ptr<std::thread> m_AudioThread;
-	std::atomic<bool> m_IsPendingExit;
-	std::atomic<bool> m_IsInitialized;
-};
 
 std::shared_ptr<iAudioSubsystem> CreateAudioSubsystem_OpenAL()
 {
@@ -189,6 +338,16 @@ void clAudioSubsystem_OpenAL::Start()
 
 			while ( !m_IsPendingExit )
 			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+				{
+					auto sources = this->GetLockedSources();
+
+					for (auto i : sources)
+					{
+						i->UpdateBuffers();
+					}
+				}
 			}
 
 			alcDestroyContext( m_Context );
@@ -203,6 +362,26 @@ void clAudioSubsystem_OpenAL::Start()
 	while ( !m_IsInitialized );
 }
 
+void clAudioSubsystem_OpenAL::RegisterSource( const std::shared_ptr<clAudioSource_OpenAL>& Source )
+{
+	std::unique_lock<std::mutex> Lock( m_ActiveSourcesMutex );
+
+	if ( std::find( m_ActiveSources.begin(), m_ActiveSources.end(), Source) != m_ActiveSources.end() ) return;
+
+	m_ActiveSources.push_back( Source );
+}
+
+void clAudioSubsystem_OpenAL::UnregisterSource( clAudioSource_OpenAL* Source )
+{
+	std::unique_lock<std::mutex> Lock( m_ActiveSourcesMutex );
+
+	m_ActiveSources.erase(
+		std::remove_if( m_ActiveSources.begin(), m_ActiveSources.end(),
+			[Source](const std::shared_ptr<clAudioSource_OpenAL>& Src){ return Src.get() == Source; } ),
+		m_ActiveSources.end()
+	);
+}
+
 void clAudioSubsystem_OpenAL::Stop()
 {
 	m_IsPendingExit = true;
@@ -212,7 +391,7 @@ void clAudioSubsystem_OpenAL::Stop()
 
 std::shared_ptr<iAudioSource> clAudioSubsystem_OpenAL::CreateAudioSource()
 {
-	return std::make_shared<clAudioSource_OpenAL>();
+	return std::make_shared<clAudioSource_OpenAL>( this );
 }
 
 void clAudioSubsystem_OpenAL::SetListenerGain( float Gain )
