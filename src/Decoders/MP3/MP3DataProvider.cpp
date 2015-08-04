a14d2a01de922e1cafed8d5a1c19de952574c65c
Sergey Kosarevsky <sk@linderdaum.com>
2015-08-04 13:59:38 +0200

    MP3DataProvider: decode MP3 info, setup decoding buffers

--- src/Decoders/MP3/MP3DataProvider.cpp
+++ src/Decoders/MP3/MP3DataProvider.cpp
@@ -1,13 +1,39 @@
 #include "MP3DataProvider.h"
+#include "Utils.h"
 
+extern "C"
+{
 #include "minimp3/minimp3.h"
+}
 
 clMP3DataProvider::clMP3DataProvider( const std::shared_ptr<clBlob>& Data )
 : m_Data( Data )
 , m_Format()
 , m_DecodingBuffer()
 , m_BufferUsed( 0 )
+, m_StreamPos( 0 )
+, m_InitialStreamPos( 0 )
+, m_IsEndOfStream( false )
 {
+	LoadMP3Info();
+
+	m_Format.m_NumChannels      = m_MP3Info.channels;
+	m_Format.m_SamplesPerSecond = m_MP3Info.sample_rate;
+	m_Format.m_BitsPerSample    = 16;
+}
+
+void clMP3DataProvider::LoadMP3Info()
+{
+	int byteCount = mp3_decode(
+		(mp3_decoder_t*)m_MP3Decoder,
+		m_Data ? const_cast<uint8_t*>( m_Data->GetDataPtr() ) : nullptr,
+		m_Data ? m_Data->GetDataSize() : 0,
+		(signed short*)m_DecodingBuffer.data(),
+		&m_MP3Info
+	);
+
+	m_StreamPos += byteCount;
+	m_InitialStreamPos = m_StreamPos;
 }
 
 const uint8_t* clMP3DataProvider::GetWaveData() const
--- src/Decoders/MP3/MP3DataProvider.h
+++ src/Decoders/MP3/MP3DataProvider.h
@@ -5,6 +5,11 @@
 
 #include "Decoders/iWaveDataProvider.h"
 
+extern "C"
+{
+#include "minimp3/minimp3.h"
+}
+
 class clBlob;
 
 /// MPEG Layer-III decoder
@@ -22,9 +27,20 @@ public:
 	virtual void Seek( float Seconds ) override;
 
 private:
+	void LoadMP3Info();
+
+private:
 	std::shared_ptr<clBlob> m_Data;
 	sWaveDataFormat m_Format;
 
 	std::vector<uint8_t> m_DecodingBuffer;
 	size_t m_BufferUsed;
+
+	size_t m_StreamPos;
+	size_t m_InitialStreamPos;
+	bool m_IsEndOfStream;
+
+	// minimp3 stuff
+	mp3_decoder_t m_MP3Decoder;
+	mp3_info_t m_MP3Info;
 };
