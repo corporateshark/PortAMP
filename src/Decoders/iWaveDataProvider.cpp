70da8c2e36ce90beb55c62184e425b1308a67ef4
Sergey Kosarevsky <sk@linderdaum.com>
2015-08-05 16:02:33 +0200

    Play all recognizable modules

--- src/Decoders/iWaveDataProvider.cpp
+++ src/Decoders/iWaveDataProvider.cpp
@@ -18,13 +18,57 @@ const char* GetFileExt( const char* FileName )
 #	define strcmpi strcasecmp
 #endif
 
+bool IsModule( const char* Ext )
+{
+	return (
+		( strcmpi( Ext, "mod" ) == 0 ) ||
+		( strcmpi( Ext, "s3m" ) == 0 ) ||
+		( strcmpi( Ext, "xm" ) == 0 ) ||
+		( strcmpi( Ext, "it" ) == 0 ) ||
+		( strcmpi( Ext, "669" ) == 0 ) ||
+		( strcmpi( Ext, "amf" ) == 0 ) ||
+		( strcmpi( Ext, "ams" ) == 0 ) ||
+		( strcmpi( Ext, "dbm" ) == 0 ) ||
+		( strcmpi( Ext, "dmf" ) == 0 ) ||
+		( strcmpi( Ext, "dsm" ) == 0 ) ||
+		( strcmpi( Ext, "far" ) == 0 ) ||
+		( strcmpi( Ext, "mdl" ) == 0 ) ||
+		( strcmpi( Ext, "med" ) == 0 ) ||
+		( strcmpi( Ext, "mtm" ) == 0 ) ||
+		( strcmpi( Ext, "okt" ) == 0 ) ||
+		( strcmpi( Ext, "ptm" ) == 0 ) ||
+		( strcmpi( Ext, "stm" ) == 0 ) ||
+		( strcmpi( Ext, "ult" ) == 0 ) ||
+		( strcmpi( Ext, "umx" ) == 0 ) ||
+		( strcmpi( Ext, "mt2" ) == 0 ) ||
+		( strcmpi( Ext, "psm" ) == 0 )
+		// compressed modules
+/*		||
+		( strcmpi( Ext, "mdz" ) == 0 ) ||
+		( strcmpi( Ext, "s3z" ) == 0 ) ||
+		( strcmpi( Ext, "xmz" ) == 0 ) ||
+		( strcmpi( Ext, "itz" ) == 0 ) ||
+		( strcmpi( Ext, "mdr" ) == 0 ) ||
+		( strcmpi( Ext, "s3r" ) == 0 ) ||
+		( strcmpi( Ext, "xmr" ) == 0 ) ||
+		( strcmpi( Ext, "itr" ) == 0 ) ||
+		( strcmpi( Ext, "mdgz" ) == 0 ) ||
+		( strcmpi( Ext, "s3gz" ) == 0 ) ||
+		( strcmpi( Ext, "xmgz" ) == 0 ) ||
+		( strcmpi( Ext, "itgz" ) == 0 )
+*/
+		// WAV
+//		|| ( strcmpi( Ext, "wav" ) == 0 )
+		);
+}
+
 std::shared_ptr<iWaveDataProvider> CreateWaveDataProvider( const char* FileName, const std::shared_ptr<clBlob>& Data )
 {
 	const char* Ext = GetFileExt( FileName );
 
 	if ( strcmpi( Ext, "mp3" ) == 0 ) return std::make_shared<clMP3DataProvider>( Data );
 	if ( strcmpi( Ext, "ogg" ) == 0 ) return std::make_shared<clOGGDataProvider>( Data );
-	if ( strcmpi( Ext, "xm" ) == 0 ) return std::make_shared<clModPlugDataProvider>( Data );
+	if ( IsModule( Ext ) ) return std::make_shared<clModPlugDataProvider>( Data );
 
 	auto MP3Blob = TryMP3InsideWAV( Data );
 
