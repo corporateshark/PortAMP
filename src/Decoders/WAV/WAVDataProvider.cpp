68126dfcfef2eb27700f9e83f4445aa7d9b3269c
Sergey Kosarevsky <sk@linderdaum.com>
2016-01-05 07:12:15 +0300

    WAV: handle chunks PAD, JUNK, INFO, CSET

--- src/Decoders/WAV/WAVDataProvider.cpp
+++ src/Decoders/WAV/WAVDataProvider.cpp
@@ -446,7 +446,11 @@ clWAVDataProvider::clWAVDataProvider( const std::shared_ptr<clBlob>& Data )
 				}
 				else if (
 					memcmp( LocalChunkHeader->ID, "fact", 4 ) == 0 ||
-					memcmp( LocalChunkHeader->ID, "LIST", 4 ) == 0
+					memcmp( LocalChunkHeader->ID, "LIST", 4 ) == 0 ||
+					memcmp( LocalChunkHeader->ID, "PAD ", 4 ) == 0 ||
+					memcmp( LocalChunkHeader->ID, "JUNK", 4 ) == 0 ||
+					memcmp( LocalChunkHeader->ID, "INFO", 4 ) == 0 ||
+					memcmp( LocalChunkHeader->ID, "CSET", 4 ) == 0
 				)
 				{
 					Offset += ChunkHeaderSize;
